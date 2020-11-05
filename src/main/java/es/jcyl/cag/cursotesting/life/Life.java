63fa3cdeea6982f8c336212097515d0e39adbbdf
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-05 15:14:30 +0100

    Implementacion muerte por inanicion

--- src/main/java/es/jcyl/cag/cursotesting/life/Life.java
+++ src/main/java/es/jcyl/cag/cursotesting/life/Life.java
@@ -24,9 +24,45 @@ public class Life {
 	}
 	
 	public void calcularNuevoCiclo() {
-		
+		boolean[][] nuevo = new boolean[this.tablero.length][this.tablero[0].length];
+		for (int i = 0; i < nuevo.length; i++) {
+			for (int j = 0; j < nuevo[i].length; j++) {
+				nuevo[i][j] = calcularNuevoEstado(i, j);
+			}
+		}
+		reemplazarTablero(nuevo);
 	}
 	
+	private boolean calcularNuevoEstado(int x, int y) {
+		boolean viva = estaViva(x, y);
+		int vecinos = contarVecinosVivos(x,y);
+		
+		if (viva) {
+			return vecinos >= 2 && vecinos <= 3;
+		}
+		else {
+			return vecinos == 3;
+		}
+	}
+	
+	private int contarVecinosVivos(int x, int y) {
+		int contador = 0;
+		for (int i = x - 1; i <= x+1; i++) {
+			for (int j = y-1; j<= y+1; j++) {
+				if (i == x && j == y) {
+					// No se cuenta a si misma
+					continue;
+				}
+				if (i >= 0 && j >= 0 && i < this.tablero.length && j < this.tablero[i].length) {
+					if (estaViva(i, j)) {
+						contador++;
+					}
+				}
+			}
+		}
+		return contador;
+	}
+
 	private void reemplazarTablero(boolean[][] nuevo) {
 		this.tablero = new boolean[nuevo.length][nuevo[0].length];
 		for (int i = 0; i < tablero.length; i++) {
