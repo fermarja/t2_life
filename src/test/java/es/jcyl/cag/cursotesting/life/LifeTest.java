d68e8206ea42fa1faa552c62a41a3ffd3fe83298
fermarja <fermarja@jagsc0001p142.jcyl.red>
2020-11-05 14:42:01 +0100

    Test inicialicion

--- src/test/java/es/jcyl/cag/cursotesting/life/LifeTest.java
+++ src/test/java/es/jcyl/cag/cursotesting/life/LifeTest.java
@@ -1,8 +1,23 @@
 package es.jcyl.cag.cursotesting.life;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class LifeTest {
 
 	public LifeTest() {
 		super();
+	}
+	
+	@Test
+	public void testTodoMuertoSigueMuerto() {
+		Life life = new Life(10,10);
+		life.calcularNuevoCiclo();
+		Assert.assertEquals(0, life.getCelulasVivas());	
+	}
+	@Test
+	public void inicializaTodoAMuerto() {
+		Life life = new Life(10,10);
+		Assert.assertEquals(0, life.getCelulasVivas());		
 	}
 }
